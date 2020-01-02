56e68134fbab7938591f97df31375bec8af5c994
prdioliveira <prdioliveira@gmail.com>
2020-01-02 18:52:53 -0300

    add field gitlab

--- app/sgteste_app/models/projeto_models.py
+++ app/sgteste_app/models/projeto_models.py
@@ -15,6 +15,7 @@ class Projeto(models.Model):
     status_projeto = models.ForeignKey(StatusProjeto, on_delete=models.PROTECT)
     data_conclusao = models.DateField(null=True, blank=True)
     cts_adicionais = models.IntegerField(null=True, blank=True, default=0)
+    gitlab_file = models.CharField(max_length=1500, null=True, blank=True)
 
     def __str__(self):
         return self.nome_projeto
